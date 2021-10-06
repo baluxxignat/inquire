ecb35d16fe619cf25207171eeccce499e4b131de
baluxxignat <aleksandr.pravo@gmail.com>
2021-10-06 14:58:43 +0300

    axios

--- src/services/API.js
+++ src/services/API.js
@@ -2,5 +2,10 @@ import axios from "axios";
 
 let axiosInstance =  axios.create({
     baseURL: 'https://bloggy-api.herokuapp.com',
-    headers: {'Content-Type: application/json'}
+    headers: {"Content-Type": "application/json"}
 })
+
+const getAllPosts = () => axiosInstance.get();
+const getSinglePost = (id) => axiosInstance.get(id)
+
+export {getAllPosts, getSinglePost}
